cb995ad3080c2d9c8ce2779f538c4e0390a269d9
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-01-26 22:02:28 +0100

    learn_DSA

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 """illustration of two codes doing same task, but with different time efficiency"""
 
 
-def 
+def print_even():
+    number = 2
+    while number <= 100:
+        if number % 2 == 0:
+            print(number)
+        number += 1
+
+print_even()
