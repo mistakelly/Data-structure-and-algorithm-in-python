eaa7e938c999e5c3bfeeca2bfdb5b87aec8f13d4
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-29 22:40:39 +0100

    compare tim

--- dsa_time.py
+++ dsa_time.py
@@ -1,15 +1,20 @@
 import time
 
-def sum_of_first_n(n):
+# def sum_of_first_n(n):
+#     total = 0
+#     for i in range(1, n+1):
+#         total += i
+#     return total
+#
+# # Test the function with different values of n
+# for n in [10, 100, 1000, 10000]:
+#     start_time = time.time()
+#     result = sum_of_first_n(n)
+#     end_time = time.time()
+#     print(f"n = {n}, Result = {result}, Time = {end_time - start_time} seconds")
+#
+
+for v in range(1, 11):
     total = 0
-    for i in range(1, n+1):
-        total += i
-    return total
-
-# Test the function with different values of n
-for n in [10, 100, 1000, 10000]:
-    start_time = time.time()
-    result = sum_of_first_n(n)
-    end_time = time.time()
-    print(f"n = {n}, Result = {result}, Time = {end_time - start_time} seconds")
-
+    total += v
+    print(total)
