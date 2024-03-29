501476a62b544984c3f0a60763895bee97d646e1
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-03-29 23:46:35 +0100

    tested inputs

--- dsa_time.py
+++ dsa_time.py
@@ -1,4 +1,5 @@
 import time
+import time
 
 def sum_of_first_n(n):
     total = 0
@@ -6,15 +7,10 @@ def sum_of_first_n(n):
         total += i
     return total
 
-# Test the function with different values of n
-for n in [10, 100, 1000, 10000]:
+# Test the function with larger values of n
+input_sizes = [10**i for i in range(1, 6)]  # input sizes from 10 to 100000
+for n in input_sizes:
     start_time = time.time()
     result = sum_of_first_n(n)
     end_time = time.time()
     print(f"n = {n}, Result = {result}, Time = {end_time - start_time} seconds")
-
-#
-# for v in range(1, 11):
-#     total = 0
-#     total += v
-#     print(total)
