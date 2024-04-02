3ebc00367054a13baa8ed5e0ce8260d7135e5d9e
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-04-02 20:49:44 +0100

    even_numbers

--- Array_Basics/all_even_and_save_in_another_array.py
+++ Array_Basics/all_even_and_save_in_another_array.py
@@ -1,23 +1,15 @@
 # Python program to store all even numbers from an array in another array
-# def even_element(lst):
-#     even_lst = []
-#     for i in range(len(lst)):
-#         if lst[i] % 2 == 0:
-#             even_lst.append(lst[i])
-#     return even_lst
-#
-#
-# even_list = even_element([1, 3, 4, 8, 6, 7])
-# print(even_list)
+def even_element(lst):
+    """
+    :param lst:
+    :return:  even list []
+    """
+    even_lst = []
+    for i in range(len(lst)):
+        if lst[i] % 2 == 0:
+            even_lst.append(lst[i])
+    return even_lst
 
 
-def get_middle(lst):
-    sorted_array = sorted(lst)
-    leng = len(sorted_array)
-    middle_list = leng / 2
-
-    print(sorted_array[middle_list])
-
-
-even_list = get_middle([1, 3, 4, 8, 6, 7, 8])
-# print(even_list)
+even_list = even_element([1, 3, 4, 8, 6, 7])
+print(even_list)
