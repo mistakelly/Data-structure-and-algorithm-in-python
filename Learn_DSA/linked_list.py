0057265e579c05375306a43f0b9390444b2c87ef
Kelvin Okoye <kelvinokoye413@gmail.com>
2024-06-13 12:34:56 +0100

    practiced on improving my solutions on linked list remove duplicate problem

--- Learn_DSA/linked_list.py
+++ Learn_DSA/linked_list.py
@@ -1,17 +1,19 @@
-class Node:
-    def __init__(self, data):
-        # Initialize a node with data and next pointer
-        self.data = data
-        self.next = None
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
     def __str__(self):
-        # Return a string representation of the node's data
-        return str(self.data)
+        # Return a string representation of the node's val
+        return str(self.val)
 
 class SinglyLinkedList:
     def __init__(self):
         # Initialize an empty linked list with a head pointer
-        self.head = None
+        # self.head = None
+        pass
 
     def list_size(self):
         # Helper method to get the size of the linked list
@@ -22,14 +24,14 @@ class SinglyLinkedList:
             tmp = tmp.next
         return count
 
-    def print_nodes(self):
+    def print_nodes(self, head):
         # Print the elements of the linked list
-        if self.head is None:
+        if head is None:
             print("List is Empty!!")
             return
-        tmp = self.head
+        tmp = head
         while tmp is not None:
-            print(tmp.data, end='->')
+            print(tmp.val, end='->')
             tmp = tmp.next
         print('NULL')
 
@@ -80,28 +82,75 @@ class SinglyLinkedList:
 
         # deletion of node
 
-    def del_begin(self):
-        if self.head is None:
+    def del_begin(self, head):
+        print('helllo from del begin')
+        if head is None:
             print("List is empty. Nothing to delete.")
             return
 
         # point head to node next
-        self.head = self.head.next
+        print('address of head', id(head))
+        head = head.next
+        self.print_nodes(head)
+
+        return head
+
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # HELP ME GOD.
+
+
+        if head is None:
+            return None
+        
+        tmp = head
+
+        while tmp and tmp.next:
+            if tmp.val == tmp.next.val:
+                tmp.next = tmp.next.next
+            else:
+                tmp = tmp.next
 
 
 
+        # return head
+
+# def convert_array_to_node(arr):
+#         head = None
+#         for data in reversed(arr):
+#             new_node = ListNode(data)
+
+#             if head is None:
+#                 head = new_node
+#             else:
+#                 new_node.next = head
+#                 head = new_node
+
+#         return head
+
+
+def convert_array_to_node(arr):
+        head = tmp = None
+        for data in arr:
+            new_node = ListNode(data)
+
+            if head is None:
+                head = new_node
+                tmp = head
+            else:
+                tmp.next = new_node
+                tmp = tmp.next
+
+
+        return head
 
 # Initialization and testing
 ll = SinglyLinkedList()
-n1 = Node(5)
-ll.head = n1
-n2 = Node(10)
-n1.next = n2
-n3 = Node(15)
-n2.next = n3
-n4 = Node(25)
-n3.next = n4
-# ll.insert_nth(Node(200), 1)
-# ll.insert_end(Node(500))
-ll.del_begin()
-ll.print_nodes()
+arr = [1, 1, 2, 2, 3, 3, 4, 5]
+
+
+head = convert_array_to_node(arr)
+ll.deleteDuplicates(head)
+ll.print_nodes(head)
+
+
+
